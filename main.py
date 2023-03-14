4c42da6fd99104b069b9b7355087b804ddf08b35
proximalf <proximalfuture@gmail.com>
2023-03-14 20:54:23 +0000

    add option for resolution

--- main.py
+++ main.py
@@ -8,16 +8,24 @@ import subprocess
 logger = logging.getLogger(__name__)
 
 class Config:
-    home = Path.home()
-    output_dir = home / "Downloads"
-    stdout = output_dir / "dylt-out.log"
-    stderr = output_dir / "dylt-err.log"
-    temp_video = output_dir / "video.mp4"
-    temp_audio = output_dir / "audio.mp4"
+    # Output directory
+    output_dir: Path = Path.home() / "Downloads"
+
+    # Download resolution
+    resolution_default = "1080p"
+
+    # Temporary file locations
+    temp_video: Path = output_dir / "video.mp4"
+    temp_audio: Path = output_dir / "audio.mp4"
+    
+    # Stdout and err for ffmpeg
+    stdout: Path = output_dir / "dylt-out.log"
+    stderr: Path = output_dir / "dylt-err.log"
 
 @click.command()
 @click.argument("url")
-def main(url: str) -> None:
+@click.option("resolution", "-r", default=Config.resolution_default)
+def main(url: str, resolution = Config.resolution_default) -> None:
     """
     CLI script to download youtube video in highest quality avalible.
     Video and Audio downloaded seperately, and merged using FFMPEG.
@@ -25,14 +33,14 @@ def main(url: str) -> None:
     logger.debug(f"URL: {url}")
     logger.debug(f"ouput directory: {Config.output_dir}")
 
-    output = str(Config.output_dir)
-
     yt = YouTube(url)
 
     click.secho(f"Downloading: {yt.title}", fg="green")
 
+    output = str(Config.output_dir)
+
     logger.debug("Downloading video component")
-    stream = yt.streams.filter(file_extension="mp4", res="1080p")[-1]
+    stream = yt.streams.filter(file_extension="mp4", res=resolution)[-1]
     stream.download(output, Config.temp_video.name)
 
     logger.debug("Downloading audio component")
@@ -47,7 +55,7 @@ def main(url: str) -> None:
     stderr = Config.stderr.open("w")
 
     muxed_file = Config.output_dir / yt.title
-    click.secho(f"Muxing w/ ffmpeg!\nSave Location: {muxed_file}", fg="red")
+    click.secho(f"Muxing w/ ffmpeg!\nSave Location: {muxed_file}", fg="green")
 
     cmd = f"ffmpeg -y -i '{Config.temp_audio}' -i '{Config.temp_video}' -shortest '{muxed_file}.mkv'"
     subprocess.call(cmd, shell=True, stdout=stdout, stderr=stderr)
