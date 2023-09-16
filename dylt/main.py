d66ed7bd9bdcc938f8dbf776e72a44c55bf2a717
proximalf <proximalfuture@gmail.com>
2023-09-16 12:28:32 +0100

    added timestamps, added some documentation

--- dylt/main.py
+++ dylt/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime, time
 from pathlib import Path
 import sys
 from pytube import YouTube
@@ -29,11 +30,33 @@ def mux_audio_video(video_title: str, output_dir: Path, temp_audio: Path, temp_v
     cmd = f"ffmpeg -y -i '{temp_audio}' -i '{temp_video}' -shortest '{muxed_file}.mkv'"
     subprocess.call(cmd, shell=True, stdout=stdout, stderr=stderr)
 
-def clip_audio_video(temp_audio: Path, temp_video: Path, clip_start: str, clip_end: str, minimum_duration: int = 1) -> None:
+def clip_audio_video(temp_audio: Path, temp_video: Path, clip_start: time, clip_end: time, minimum_duration: int = 1) -> VideoFileClip:
     """
     This function clips and merges the file.
+
+    Parameters
+    ----------
+    temp_audio: Path
+        Path to downloaded audio.
+    temp_video: Path
+        Path to downloaded video.
+    clip_start: time
+        Start time as a time object of clip.
+    clip_end: time
+        End time as a time object of clip.
+    minimum_duration: int
+        Minimum duration a video can be, in seconds, default is 1.
+
+    Returns
+    ----------
+    video_clip: VideoFileClip
+        Video clip with audio attached.
     """
-    if clip_start < minimum_duration:
+    # Convert from time objects to seconds
+    time2seconds = lambda x: int((x.minute * 60) + x.second)
+    clip_start, clip_end = time2seconds(clip_start), time2seconds(clip_end)
+
+    if clip_start< minimum_duration:
         logger.error(f"Duration too short: {clip_start} < {minimum_duration}")
     
     logger.debug(f"Clipping - a:{temp_audio}, v:{temp_video}")
@@ -43,9 +66,9 @@ def clip_audio_video(temp_audio: Path, temp_video: Path, clip_start: str, clip_e
     video_file = VideoFileClip(str(temp_video))
     video_clip = video_file.subclip(clip_start, clip_end)
     
+    # Add clipped audio
     video_clip.audio = audio_clip
-    logger.debug("Merging and saving clip...")    
-    
+
     return video_clip
 
 
@@ -73,7 +96,18 @@ def cli(url: str, resolution: str = Config.resolution_default, clip: Optional[st
     """
     CLI script to download youtube video in highest quality avalible.
     Video and Audio downloaded seperately, and merged using FFMPEG.
-    Clipping is accepted in the format xx,yy in seconds (start, end).
+    Clipping is accepted in isoformat HH:MM:SS (start, end), eg `-c 4:04,5:23`.
+
+    Parameters
+    ----------
+    url: str
+        String of youtube url link.
+    resolution: str 
+        Using config default, but can be set.
+    clip: Optional[str]
+        Set a time stamp to clip from. This must inlude start&end timestamps.
+    sysout_logging: bool
+        Flag to set wether details are logged using sysout.
     """
     logger.setLevel(logging.DEBUG)
     if sysout_logging:
@@ -115,13 +149,16 @@ def cli(url: str, resolution: str = Config.resolution_default, clip: Optional[st
     click.secho("Download complete!", fg="green")
 
     if clip is not None:
-        clip_start, clip_end = [int(i) for i in clip.split(",")]
+        clip_start, clip_end = [datetime.strptime(i, "%M:%S").time() for i in clip.split(",")]
         click.secho(f"Clipping... {clip_start} - {clip_end}")
         video_clip = clip_audio_video(
             Config.temp_audio,
             Config.temp_video,
             clip_start, clip_end
         )
+
+        logger.debug("Merging and saving clip...")  
+
         save_clipped_video(
             video_clip,
             Config.output_dir / f"{yt.title}.mp4",
